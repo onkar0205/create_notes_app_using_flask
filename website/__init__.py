51b70964437daaeeed648dba1d2dfceab72d4e26
onkar0205 <onkarc.naik02@gmail.com>
2024-06-02 11:56:48 +0530

    updated __init__

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from .views import views
+from .auth import auth
+from .models import User, Note
  
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -13,13 +16,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-    from .views import views
-    from .auth import auth
-
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-
-    from .models import User, Note
     
     with app.app_context():
         db.create_all()
